445e814d1366698436310a925ffa33bcf1581d4d
matt969696 <2531@holbertonschool.com>
2021-12-01 10:28:12 +0000

    Fix() : Change variable names

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -5,7 +5,7 @@ This module contains a simple function
 
 
 def np_elementwise(mat1, mat2):
-    """ performs element-wise addition, subtraction, mult and division"""
+    """performs element-wise calculus"""
     madd = mat1 + mat2
     msub = mat1 - mat2
     mmul = mat1 * mat2
