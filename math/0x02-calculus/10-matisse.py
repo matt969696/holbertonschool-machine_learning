006409834cc7431281b968dee298cd5140d7209b
matt969696 <2531@holbertonschool.com>
2021-12-08 12:18:21 +0000

    fix() : add limit case for 10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,9 +6,9 @@ This module contains a simple function
 
 def poly_derivative(poly):
     """calculates the derivative of a polynomial"""
-    if not isinstance(poly, list):
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
-    if len(poly) <= 1:
+    if len(poly) == 1:
         return [0]
     if not all(isinstance(e, (int, float)) for e in poly):
         return None
