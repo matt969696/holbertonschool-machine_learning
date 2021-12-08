20db2e827bee383dca069a20dddcbca602768d7b
matt969696 <2531@holbertonschool.com>
2021-12-08 12:24:34 +0000

    fix() : add limit case for 17

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,9 +6,13 @@ This module contains a simple function
 
 def poly_integral(poly, C=0):
     """calculates the integral of a polynomial"""
-    if not isinstance(poly, list):
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
     if not all(isinstance(e, (int, float)) for e in poly):
         return None
+    if not isinstance(C, (int, float)):
+        return None
     integ = [C] + [poly[i] / (i + 1) for i in range(len(poly))]
+    while integ[-1] == 0 and len(integ) > 1:
+        integ.pop()
     return integ
