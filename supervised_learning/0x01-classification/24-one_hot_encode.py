310a77a6474aeb5b1579707ce4190b18281d3177
matt969696 <matt96@melix.net>
2022-01-03 10:55:58 +0000

    fix() : add try except

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -7,16 +7,14 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """converts a numeric label vector into a one-hot matrix"""
+    if type(Y) is not np.ndarray:
+        return None
     if type(classes) is not int:
         return None
     if len(Y.shape) != 1:
         return None
-    if classes <= 0:
-        return None
-    if type(Y) is not np.ndarray:
-        return None
     try:
-        ret = np.eye(classes)[Y]
-        return ret.T
+        hot = np.eye(classes)[Y]
+        return hot.T
     except Exception:
         return None
