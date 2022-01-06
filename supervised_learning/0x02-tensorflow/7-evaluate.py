4f31def87962c08c295f966869073f5f7e4f3903
matt969696 <matt96@melix.net>
2022-01-06 14:11:17 +0000

    feat() : add initial solutions

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This module contains a simple function
+"""
+
+import tensorflow.compat.v1 as tf
+
+
+def evaluate(X, Y, save_path):
+    """evaluates the output of a neural network"""
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph("{}.meta".format(save_path))
+        saver.restore(sess, save_path)
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+        y_ = sess.run(y_pred, {x: X, y: Y})
+        acc = sess.run(accuracy, {x: X, y: Y})
+        loss = sess.run(loss, {x: X, y: Y})
+        return y_, acc, loss
