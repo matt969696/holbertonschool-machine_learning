ed2fd97b506e74455657b38bee756e8de1aeebc3
matt969696 <matt96@melix.net>
2022-01-06 14:31:11 +0000

    feat() : add initial solutions

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -9,8 +9,6 @@ import tensorflow.compat.v1 as tf
 def evaluate(X, Y, save_path):
     """evaluates the output of a neural network"""
     with tf.Session() as sess:
-        all_variables = tf.get_collection_ref(tf.GraphKeys.GLOBAL_VARIABLES)
-        sess.run(tf.variables_initializer(all_variables))
         saver = tf.train.import_meta_graph("{}.meta".format(save_path))
         saver.restore(sess, save_path)
         x = tf.get_collection("x")[0]
@@ -20,5 +18,5 @@ def evaluate(X, Y, save_path):
         loss = tf.get_collection("loss")[0]
         y_ = sess.run(y_pred, {x: X, y: Y})
         acc = sess.run(accuracy, {x: X, y: Y})
-        loss = sess.run(loss, {x: X, y: Y})
-        return y_, acc, loss
+        loss_eval = sess.run(loss, {x: X, y: Y})
+        return y_, acc, loss_eval
