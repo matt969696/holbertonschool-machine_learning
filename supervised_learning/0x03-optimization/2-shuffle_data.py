54370aba1c021cd48a0878351e587af214cf8933
matt969696 <2531@holbertonschool.com>
2022-01-12 12:40:27 +0000

    fix() : add seed in 2

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -8,5 +8,6 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """shuffles the data points in two matrices the same way"""
+    np.random.seed(0)
     p = np.random.permutation(X.shape[0])
     return X[p], Y[p]
