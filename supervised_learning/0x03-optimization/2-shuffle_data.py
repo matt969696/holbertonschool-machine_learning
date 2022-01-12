6a94b1f371b82abad68721482f37a1fa8efd7b9c
matt969696 <2531@holbertonschool.com>
2022-01-12 10:19:53 +0000

    feat() : small correction on 2

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -8,5 +8,5 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """shuffles the data points in two matrices the same way"""
-    p = np.random.permutation(len(X))
+    p = np.random.permutation(X.shape[0])
     return X[p], Y[p]
